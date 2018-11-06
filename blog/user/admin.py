84cc651fc31cd3c2121ed3e49a36191eedd9a9d1
Zhoucwabc <44575680+Zhoucwabc@users.noreply.github.com>
2018-11-06 12:51:10 +0800

    Update admin.py

--- blog/user/admin.py
+++ blog/user/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('nickname','uname','upwd','uemail','isAuthor','isActive')
+    list_editable = ('uname','upwd','uemail','isAuthor','isActive')
+
+
+admin.site.register(User,UserAdmin)
