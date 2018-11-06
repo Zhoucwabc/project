a9d16512ed1dc2741a81e1b00dd326145c58bcf8
Zhoucwabc <44575680+Zhoucwabc@users.noreply.github.com>
2018-11-06 12:50:42 +0800

    Update models.py

--- blog/user/models.py
+++ blog/user/models.py
@@ -7,10 +7,11 @@ class User(models.Model):
     uname = models.CharField(max_length=20)
     upwd = models.CharField(max_length=20)
     uemail = models.EmailField(null=True)
+    isAuthor = models.BooleanField(default=False)
     isActive = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.name
-    
+        return self.nickname
+
     def Meta(self):
-        db_table = 'user'
+        db_table = 'nickname'
